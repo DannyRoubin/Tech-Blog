0827edb69b4c530c0fe666f24c445a431eeae797
Danny Roubin <dannyroubin1@gmail.com>
2021-07-29 22:35:30 -0700

    finally got the blog id to be read by the js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,29 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
+  const blogId = document.getElementById("submitbtn").getAttribute("data-id");
 
-  const blogId = event.target.getAttribute("data-id");
   const comment = document.querySelector("#comment-desc").value.trim();
 
-  if (comment) {
-    const response = await fetch(`/api/comments`, {
-      method: "POST",
-      body: JSON.stringify({ comment, blogId }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  console.log(blogId);
+  console.log(comment);
 
-    if (response.ok) {
-      document.location.assign(`/api/blogs/${blogId}`);
-    } else {
-      alert("Failed to create comment");
-    }
-  }
+  //   if (comment) {
+  //     const response = await fetch(`/api/comments`, {
+  //       method: "POST",
+  //       body: JSON.stringify({ comment, blogId }),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+
+  //     if (response.ok) {
+  //       document.location.assign(`/api/blogs/${blogId}`);
+  //     } else {
+  //       alert("Failed to create comment");
+  //     }
+  //   }
 };
+
+document
+  .querySelector(".new-comment-form")
+  .addEventListener("submit", newFormHandler);
--- public/js/profile.js
+++ public/js/profile.js
@@ -24,16 +24,17 @@ const newFormHandler = async (event) => {
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute("data-id")) {
     const id = event.target.getAttribute("data-id");
+    console.log(id);
 
-    const response = await fetch(`/api/blogs/${id}`, {
-      method: "DELETE",
-    });
+    // const response = await fetch(`/api/blogs/${id}`, {
+    //   method: "DELETE",
+    // });
 
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to delete blog");
-    }
+    // if (response.ok) {
+    //   document.location.replace("/profile");
+    // } else {
+    //   alert("Failed to delete blog");
+    // }
   }
 };
 
