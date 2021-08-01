f5dc284533c35125ea4b4756e5dfa39c33c10ff4
Danny Roubin <dannyroubin1@gmail.com>
2021-07-31 22:07:54 -0700

    users are now able to add comments and view their comments on their profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,27 @@
-const newFormHandler = async (event) => {
+const newBlogHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const blog_title = document.querySelector("#blog_title").value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+  const blog_body = document.querySelector("#blog_body").value.trim();
+
+  if (blog_title && blog_body) {
+    const response = await fetch(`/api/blog`, {
+      method: "POST",
+      body: JSON.stringify({ blog_title, blog_body }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to create project');
-    }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
+      alert("Failed to create blog");
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".new-blog-form")
+  .addEventListener("submit", newBlogHandler);
