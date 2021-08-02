6c59e1dc71f551f818c6a65e77cb6828f647ad03
Danny Roubin <dannyroubin1@gmail.com>
2021-08-02 16:50:56 -0700

    cont fixing deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -18,6 +18,9 @@ const sess = {
   secret: "Super secret secret",
   cookie: {
     maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
@@ -38,7 +41,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log("Now listening");
-  sequelize.sync({ force: false });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
