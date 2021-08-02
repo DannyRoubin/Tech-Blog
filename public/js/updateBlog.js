f2607453614ba4ae813c9a12b44f6e9a6c86e69e
Danny Roubin <dannyroubin1@gmail.com>
2021-08-02 12:45:53 -0700

    users are now able to update and delete their own blogs and seed data was changed as well to match the readme example

---
+++ public/js/updateBlog.js
@@ -0,0 +1,61 @@
+const updateBlogHandler = async (event) => {
+  event.preventDefault();
+
+  const blog_title = document.querySelector("#blog_title").value.trim();
+  const blog_body = document.querySelector("#blog_body").value.trim();
+
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/update/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ blog_title, blog_body }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to update blog");
+    }
+  }
+};
+
+const deleteBlogHandler = async (event) => {
+  event.preventDefault();
+
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/update/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to delete blog");
+    }
+  }
+};
+
+const cancelBtnHandler = async (event) => {
+  event.preventDefault();
+  if (event.target.hasAttribute("data-id")) {
+    document.location.replace("/profile");
+  }
+};
+
+document
+  .querySelector("#updatebtn")
+  .addEventListener("click", updateBlogHandler);
+
+document
+  .querySelector("#deletebtn")
+  .addEventListener("click", deleteBlogHandler);
+
+document
+  .querySelector("#cancelbtn")
+  .addEventListener("click", cancelBtnHandler);
